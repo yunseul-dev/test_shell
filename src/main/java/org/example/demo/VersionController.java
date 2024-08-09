775a19575496a74082f08dc6b4592948e513f118
yunseul <siddl333@naver.com>
2024-08-09 14:35:03 +0900

    [Init] first commit

---
+++ src/main/java/org/example/demo/VersionController.java
@@ -0,0 +1,15 @@
+package org.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok("v1");
+    }
+}
